37a1baa8e236c41f028fb0126b67649c2a798472
Zyzzikito <98108675+Zyzzikito@users.noreply.github.com>
2022-01-26 19:58:47 +0300

    B4bra

--- laba3.py
+++ laba3.py
@@ -1,13 +1,15 @@
 #бери Х как переменную ввода
 
-def x2():
+def x2(x):
+  x/= 2
+    return collatz(x)
 
-
-def x3_1():
-
+def x3_1(x):
+  x = x*3 + 1
+    return collatz(x)
+  
 #коллатц мой
 def collatz(x):
 
   
   if __name__ == '__main__':
-  
